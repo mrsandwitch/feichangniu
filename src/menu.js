dcde3df655a9959d9e62b207532d800201702b59
mrsandwitch <mrsandwitch@gmail.com>
2016-11-14 07:41:55 +0800

    version with articulation

--- src/menu.js
+++ src/menu.js
@@ -10,11 +10,17 @@ function onOpen() {
       .addItem('test', 'testFunc')
       .addToUi();
 }
-//-----------------------------------------------/
 
-function returnMainSheet() {
-	var ss = SpreadsheetApp.getActiveSpreadsheet();
-	var mainSheetName = "成本分析總表";
-	var sheet = ss.getSheetByName(mainSheetName);
-	ss.setActiveSheet(sheet).setActiveSelection("A1");
+function onOpen() {
+	  var ui = SpreadsheetApp.getUi();
+	  // Or DocumentApp or FormApp.
+	  ui.createMenu('非常牛指令')
+		  .addItem('返回總表', 'returnMainSheet')
+		  .addSeparator()
+		  .addItem('進入公司細項', 'jumpToCompSheet')
+		  .addSeparator()
+		  .addItem('勾稽', 'allArticulaton')
+		  .addSeparator()
+		  .addItem('產生公司分頁', 'genCompTab')
+		  .addToUi();
 }
--- src/pageJump.js
+++ src/pageJump.js
@@ -1,8 +1,15 @@
 
-
 function returnMainSheet() {
 	var ss = SpreadsheetApp.getActiveSpreadsheet();
 	var mainSheetName = "成本分析總表";
 	var sheet = ss.getSheetByName(mainSheetName);
+	ss.setActiveSheet(sheet).setActiveSelection("A1");
+}
+
+function jumpToCompSheet() {
+	var ss = SpreadsheetApp.getActiveSpreadsheet();
+	var selRange = ss.getActiveRange();  
+	var selRangeData = selRange.getValue();  
+	var sheet = ss.getSheetByName(selRangeData);
 	ss.setActiveSheet(sheet).setActiveSelection("A1");
 }
